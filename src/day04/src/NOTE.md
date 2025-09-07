cea3901a3471e5a115c156a362b2283603d47ca2
LuckyQu <super.lucky.qu@gmail.com>
2025-09-07 13:38:27 +0800

    learn: 初识react-router

---
+++ src/day04/src/NOTE.md
@@ -0,0 +1,7 @@
+# React-Router学习
+
+## 用处
+将不同的访问URL对应渲染不同的组件
+
+## 引入React-Router-Dom
+npm i react-router-dom
--- src/day04/src/main.tsx
+++ src/day04/src/main.tsx
@@ -1,6 +1,28 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+//引入createBrowserRouter函数和RouterProvider组件
+//然后将路由与组件进行绑定
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom"
 
+const router = createBrowserRouter(
+    [
+        {
+            path: "/login",
+            element:<div>我是登陆页</div>
+        },
+        {
+            path: "/register",
+            element:<div>我是注册页</div>
+        },
+        {
+            path:"/",
+            element:<App />
+        }
+    ]
+)
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={router}></RouterProvider>
 )
