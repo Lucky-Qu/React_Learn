e8b323d3d3a8b201526d95f84c969dd9473809bf
LuckyQu <super.lucky.qu@gmail.com>
2025-09-11 10:19:54 +0800

    learn: 学习Memo

--- src/day06/src/components/DoNotReRender/index.tsx
+++ src/day06/src/components/DoNotReRender/index.tsx
@@ -1,12 +1,8 @@
-const DoNotReRenderMe = (props: number) => {
-    console.log("我被渲染了！")
+const DoNotReRenderMe = () => {
     return (
         <>
             <div>
-                props:{props}
-            </div>
-            <div>
-                我是一个不需要被二次渲染的组件，除非我的props改变
+                我是一个不需要被二次渲染的组件
             </div>
         </>
     )
--- src/day06/src/pages/ReactMemo/index.tsx
+++ src/day06/src/pages/ReactMemo/index.tsx
@@ -1,8 +1,28 @@
 import BackToNavi from "../../components/BackToNavi/BackToNavi.tsx";
+import DoNotReRenderMe from "../../components/DoNotReRender";
+import {memo, useState} from "react";
 
 const ReactMemo = () => {
+    const MemoDoNotReRenderMe = memo(DoNotReRenderMe)
+    const [num, setNum] = useState(0)
+    console.log("父组件被重新渲染了！")
     return (
         <>
+            <div>
+                使用memo函数包裹在父组件被重新渲染时不需要被重新渲染的子组件即可
+            </div>
+            <div>
+                他会返回一个新的组件，使用新的组件即可
+            </div>
+            <div>
+                如果有props，在props改变的时候会重新渲染子组件
+            </div>
+            <div>
+                react会使用Object.Is来比较，全部返回true才会跳过渲染，对于复杂类型，只要引用类型发生改变，就判断为改变
+            </div>
+            <MemoDoNotReRenderMe />
+            <div>子组件被渲染时，控制台会打印，使用一个状态变量来让父组件重新渲染</div>
+            <button onClick={() => {setNum(num + 1)}}>重新渲染父组件</button>
             <BackToNavi />
         </>
     )
